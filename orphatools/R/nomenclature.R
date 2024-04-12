2b71426d35bfc10b4d1efc2274491581ca6a8ca2
Rémy Dumas <remy.dumas@aphp.fr>
2024-04-12 14:25:05 +0200

    added ORPHAcodes-genes relationships

--- orphatools/R/nomenclature.R
+++ orphatools/R/nomenclature.R
@@ -23,11 +23,14 @@ classLevel_literal = c('36540'='Group',
 
 #' Update the Orpha nomenclature pack. Orphanet updates its pack on a yearly basis.
 #'
-#' @param orpha_pack_nomenclature_path the path to the uncompressed nomenclature pack.
-#' You'll find it available here : https://www.orphadata.com/pack-nomenclature/
+#' @param orpha_pack_nomenclature_path the path to the uncompressed nomenclature pack,
+#' available here: https://www.orphadata.com/pack-nomenclature/
 #' @param nomenclature_xml_file nomenclature file is supposed to be _ORPHAnomenclature_fr.xml_, but might differ.
 #' @param classifications_folder classifications folder is supposed to be _Classifications_fr_, but might differ
 #'
+#' @import magrittr
+#' @importFrom xml2 read_xml as_list
+#' @importFrom dplyr bind_rows
 #' @return
 #' @export
 #'
@@ -122,9 +125,6 @@ update_nomenclature_pack = function(orpha_pack_nomenclature_path,
 #'
 #' @return The nomenclature data as a data.frame object.
 #'
-#' @import magrittr
-#' @importFrom xml2 read_xml as_list
-#' @importFrom dplyr bind_rows
 #' @export
 #' @examples
 #' nom_data = load_nomenclature()
@@ -146,7 +146,7 @@ See `update_nomenclature_pack` or consider reisntalling orphatools package.'))
 
 #' Load for each orphaCode its list of synonyms from the nomenclature data
 #'
-#' @return A data.frame object with three columns : orpha Code, preferred term, synonyms
+#' @return A data.frame object with three columns : ORPHAcode, preferred term, synonyms
 #' @export
 #'
 #' @examples
@@ -165,10 +165,10 @@ See `update_nomenclature_pack` or consider reisntalling orphatools package.'))
   return(df_synonyms)
 }
 
-#' Load for each obsolete or deprecated ORPHA code the corresponding association,
-#' which means the ORPHA code it was moved to or referred to
+#' Load for each obsolete or deprecated ORPHAcode the corresponding association,
+#' which means the ORPHAcode it was moved to or referred to
 #'
-#' @return A data.frame object giving the inactive ORPHA codes, the corresponding associations with the association type
+#' @return A data.frame object giving the inactive ORPHAcodes, the corresponding associations with the association type
 #' @export
 #'
 #' @examples
@@ -184,6 +184,154 @@ load_associations = function(){
 See `update_nomenclature_pack` or consider reisntalling orphatools package.'))
 
   return(df_associations)
+}
+
+#' Update genes information from orphanet public data,
+#' available here: https://www.orphadata.com/genes/
+#'
+#' @param genes_file An xml file published by Orphanet
+#'
+#' @import magrittr
+#' @importFrom dplyr bind_rows distinct left_join mutate across
+#' @importFrom tidyr pivot_wider
+#' @importFrom stringr str_split_1
+#'
+#'
+#' @export
+update_genes = function(genes_file){
+  adaptative_to_list = function(y){
+    if(is.na(y)) return(NULL)
+    else return(str_split_1(y, ','))
+  }
+
+  find_orpha_genes = function(disorder_node){
+    props_df = data.frame(
+      orphaCode = disorder_node$OrphaCode[[1]],
+
+      gene_id = sapply(disorder_node$DisorderGeneAssociationList, \(x) attr(x$Gene, 'id')) %>% unname(),
+      associationType = sapply(disorder_node$DisorderGeneAssociationList, function(x)  x$DisorderGeneAssociationType$Name[[1]]) %>% unname(),
+      associationStatus = sapply(disorder_node$DisorderGeneAssociationList, function(x)  x$DisorderGeneAssociationStatus$Name[[1]]) %>% unname()
+      )
+
+    return(props_df)
+  }
+
+  find_gene_synonyms = function(gene_node){
+    props_df = data.frame(
+      gene_id = attr(gene_node$Gene, 'id'),
+      n_syns = gene_node$Gene$SynonymList %>% attr('count') %>% as.numeric(),
+      pref_symbol = gene_node$Gene$Symbol[[1]],
+      name = gene_node$Gene$Name[[1]],
+      synonyms = sapply(gene_node$Gene$SynonymList, \(x) x[[1]]) %>% unname()) %>%
+      mutate(synonyms = ifelse(n_syns, synonyms, as.character(NA))) %>%
+      select(-n_syns)
+    return(props_df)
+  }
+
+  find_gene_extrefs = function(gene_node){
+    if(attr(gene_node$Gene$ExternalReferenceList, 'count') != "0")
+      props_df = data.frame(
+        gene_id = attr(gene_node$Gene, 'id'),
+        source = sapply(gene_node$Gene$ExternalReferenceList, \(x) x$Source[[1]]) %>% unname(),
+        reference = sapply(gene_node$Gene$ExternalReferenceList, \(x) x$Reference[[1]]) %>% unname())
+    else
+      return(NULL)
+
+    return(props_df)
+  }
+
+  find_gene_locus = function(gene_node){
+    if(attr(gene_node$Gene$LocusList, 'count') != "0")
+      props_df = data.frame(
+        gene_id = attr(gene_node$Gene, 'id'),
+        locus = sapply(gene_node$Gene$LocusList, \(x) x$GeneLocus[[1]]) %>% unname())
+    else
+      # return(NULL)
+      props_df = data.frame(
+        gene_id = attr(gene_node$Gene, 'id'),
+        locus = as.character(NA))
+
+    return(props_df)
+  }
+
+  if(file.exists(genes_file)){
+    # Load raw data from xml files
+    genes_data_raw = read_xml(genes_file, encoding = 'ISO-8859-1') %>% as_list()
+
+    # Preprocess data to make it R-manageable
+    genes_synonyms = genes_data_raw[[1]][[2]] %>%
+      lapply(\(disorder_node)
+             lapply(disorder_node$DisorderGeneAssociationList, find_gene_synonyms) %>% bind_rows()) %>%
+      bind_rows() %>%
+      distinct()
+
+    genes_extrefs = genes_data_raw[[1]][[2]] %>%
+      lapply(\(disorder_node)
+             lapply(disorder_node$DisorderGeneAssociationList, find_gene_extrefs) %>% bind_rows()) %>%
+      bind_rows() %>%
+      distinct()
+
+    genes_locus = genes_data_raw[[1]][[2]] %>%
+      lapply(\(disorder_node)
+             lapply(disorder_node$DisorderGeneAssociationList, find_gene_locus) %>% bind_rows()) %>%
+      bind_rows() %>%
+      distinct()
+
+    orpha_genes = genes_data_raw[[1]][[2]] %>%
+      lapply(find_orpha_genes) %>%
+      bind_rows() %>%
+      distinct() %>%
+      left_join(genes_synonyms %>% select(gene_id, pref_symbol, name) %>% distinct(), by='gene_id') %>%
+      left_join(genes_extrefs %>% pivot_wider(names_from=source, values_from=reference, values_fn=\(x) paste0(x, collapse = ',')), by='gene_id') %>%
+      mutate(across(c(OMIM, Reactome, SwissProt), ~ sapply(.x, adaptative_to_list, USE.NAMES = F))) %>%
+      left_join(genes_locus, by='gene_id')
+
+    # Save changes
+    extdata_path = system.file('extdata', package='orphatools')
+    saveRDS(orpha_genes, file.path(extdata_path, 'orpha_genes.RDS'))
+    saveRDS(genes_synonyms, file.path(extdata_path, 'genes_synonyms.RDS'))
+  }
+  else
+    stop(simpleError('Given file is invalid.'))
+}
+
+#' Load the list of genes provided by Orphanet and their associated ORPHAcodes
+#'
+#' @return A data frame containing for each ORPHAcode the kind of association (if any)
+#' with a related gene and all known information (symbol, name, references, locus) about the latter.
+#' @export
+load_orpha_genes = function(){
+  extdata_path = system.file('extdata', package='orphatools')
+  orpha_genes_path = file.path(extdata_path, 'orpha_genes.RDS')
+
+  if(file.exists(orpha_genes_path))
+    df_orpha_genes = readRDS(orpha_genes_path)
+  else
+    stop(simpleError(
+    'Loading of associations failed. Internal files might be broken.
+    See `update_nomenclature_pack` or consider reisntalling orphatools package.'))
+
+  return(df_orpha_genes)
+}
+
+#' Load all possible synonyms for each known gene in Orphanet public data
+#'
+#' @return A data frame containing genes ids for joining operations, their full name and main symbol and all possible synonyms
+#' @export
+#'
+#' @examples
+load_genes_synonyms = function(){
+  extdata_path = system.file('extdata', package='orphatools')
+  genes_synonyms_path = file.path(extdata_path, 'genes_synonyms.RDS')
+
+  if(file.exists(genes_synonyms_path))
+    df_genes_synonyms = readRDS(genes_synonyms_path)
+  else
+    stop(simpleError(
+    'Loading of associations failed. Internal files might be broken.
+    See `update_nomenclature_pack` or consider reisntalling orphatools package.'))
+
+  return(df_genes_synonyms)
 }
 
 
@@ -213,9 +361,9 @@ translate_properties = function(nom_data){
 }
 
 
-#' Get the flag value of a given ORPHA code
+#' Get the flag value of a given ORPHAcode
 #'
-#' @param orphaCode, ORPHA code to get the flag value from
+#' @param orphaCode, ORPHAcode to get the flag value from
 #'
 #' @return The corresponding flag value
 #' @export
@@ -228,9 +376,9 @@ get_flag_value = function(code){
   return(flagValue)
 }
 
-#' Get the disorder type of a given ORPHA code
+#' Get the disorder type of a given ORPHAcode
 #'
-#' @param orphaCode, ORPHA codeto get the disorder type from
+#' @param orphaCode, ORPHAcodeto get the disorder type from
 #'
 #' @return The corresponding disorder type
 #' @export
@@ -244,9 +392,9 @@ get_disorder_type = function(code){
 }
 
 
-#' Get the classification level of a given ORPHA code
+#' Get the classification level of a given ORPHAcode
 #'
-#' @param orphaCode, ORPHA code to get the classification level from
+#' @param orphaCode, ORPHAcode to get the classification level from
 #'
 #' @return The corresponding classification level
 #' @export
